92851970c1c80d1d1d93ce44b3007942a84168af
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-03-18 06:41:17 +0200

    add constants file

---
+++ src/constants/styles.constants.ts
@@ -0,0 +1,15 @@
+export const color_primary = '';
+export const color_secondary = '';
+
+export const color_app_background = '#fff';
+
+export const color_input_placeholder = '#878787';
+export const color_input = '';
+
+export const color_pure_black = '#000';
+export const color_pure_white = '#fff';
+
+export const color_black = '#151515';
+export const color_white = '#FDFDFD';
+
+export const color_light_grey = '#F8F8F8';
