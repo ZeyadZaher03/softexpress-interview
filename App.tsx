3d33117771f83d43fbfeb2e650f7b2910213356d
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-03-18 20:51:06 +0200

    App init

--- App.tsx
+++ App.tsx
@@ -1,27 +1,31 @@
 import React, {Component} from 'react';
-import Search from './src/screens/search/search.screen';
-import Details from './src/screens/details/details.screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-interface AppProps {}
+import SearchScreen from './src/screens/search/search.screen';
+import Details from './src/screens/details/details.screen';
 
-interface AppState {
-  isDarkMode: boolean;
-}
+type RootStackParamList = {
+  Search: {
+    component: any;
+  };
+  Details: {};
+};
 
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-  }
-
+class App extends Component {
   render() {
-    const Stack = createNativeStackNavigator();
+    const Stack = createNativeStackNavigator<RootStackParamList>();
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Search" component={Search} />
-          <Stack.Screen name="Details" component={Details} />
+          <Stack.Screen
+            name="Search"
+            component={SearchScreen as React.ComponentType<any>}
+          />
+          <Stack.Screen
+            name="Details"
+            component={Details as React.ComponentType<any>}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
