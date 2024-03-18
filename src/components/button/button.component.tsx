0ca05aeaf7409470d48a9c6cd546998bed183c21
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-03-18 20:15:47 +0200

    create Button [Presentational Component]

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {styles} from './button.styles';
+import {TextStyle, TouchableOpacity, ViewStyle} from 'react-native';
+import Text from '../text/text.component';
+
+interface ButtonProps {
+  children: string;
+  customStyle?: ViewStyle;
+  customTextStyle?: TextStyle;
+  onPress?: () => {};
+}
+
+class Button extends Component<ButtonProps> {
+  render() {
+    const {onPress, children, customStyle, customTextStyle} = this.props;
+
+    return (
+      <TouchableOpacity
+        style={{...styles.button, ...customStyle}}
+        onPress={onPress}>
+        <Text customStyles={customTextStyle}>{children}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default Button;
