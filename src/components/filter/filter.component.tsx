bde4269a1cbc16c58da0deaeaab6118c7c0f7114
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-03-18 20:17:59 +0200

    filter

---
+++ src/components/filter/filter.component.tsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+import {ViewStyle} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native';
+import Pill from '../pill/pill.component';
+
+interface List {
+  label: string;
+  id: number;
+  selected: boolean;
+  value: string;
+}
+
+interface FilterProps {
+  list: List[];
+  onPress: (item: List) => void;
+  customStyles?: ViewStyle;
+}
+
+interface FilterState {}
+
+class Filter extends Component<FilterProps, FilterState> {
+  render() {
+    const {list, onPress, customStyles} = this.props;
+
+    return (
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={customStyles}
+        horizontal={true}>
+        {list.map(item => (
+          <TouchableOpacity key={item.id} onPress={() => onPress(item)}>
+            <Pill selected={item.selected}>{item.label}</Pill>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    );
+  }
+}
+
+export default Filter;
