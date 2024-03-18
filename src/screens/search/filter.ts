91ae66922e7a976ff63c77cba5ef4ea655060623
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-03-18 20:50:24 +0200

    filters constant

---
+++ src/screens/search/filter.ts
@@ -0,0 +1,22 @@
+export const FILTERS_LIST = [
+  {
+    label: 'All',
+    value: 'all',
+    id: 0,
+  },
+  {
+    label: 'low sugar',
+    value: 'low-sugar',
+    id: 1,
+  },
+  {
+    label: 'keto',
+    value: 'keto-friendly',
+    id: 2,
+  },
+  {
+    label: 'vegan',
+    value: 'vegan',
+    id: 3,
+  },
+];
